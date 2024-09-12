8e2af2ef052d9a9607850f239b10433f4655bce3
mirongdev <mirongdev@gmail.com>
2024-09-12 10:43:58 +0700

    auto

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('wa engine by mirongdev vercel bisa');
+  res.send('wa engine by mirongdev vercel bisa mongose');
 });
 app.get('/cek', (req, res) => {
     res.send('perubahan menunggu proses build selesai yah');
